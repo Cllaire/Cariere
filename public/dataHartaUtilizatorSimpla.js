c7358b16159095ac504bc126be901cf843f151d8
Alexandra Balan <alexandra.balan10@gmail.com>
2018-05-20 22:51:03 +0300

    final(sper)

--- public/dataHartaUtilizatorSimpla.js
+++ public/dataHartaUtilizatorSimpla.js
@@ -4,7 +4,75 @@ function initApp() {
 	document.getElementById('ButonSignOut').addEventListener('click', ButonSignOut, false);
 	document.getElementById('ButonProgramPrezentari').addEventListener('click', ButonProgramPrezentari, false);
 	document.getElementById('ButonProgramWorkshopuri').addEventListener('click', ButonProgramWorkshopuri, false);
+	colecteazaButoane();
+	colecteazaButoane2();
+	colecteazaButoane3();
+	
 }
+
+
+
+function colecteazaButoane() {
+
+	var container1 = document.getElementsByClassName("containerHarti1")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '1' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
+ 
+ 
+ function colecteazaButoane2() {
+ 
+	var container1 = document.getElementsByClassName("containerHarti2")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '2' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
+ 
+ function colecteazaButoane3() {
+ 
+	var container1 = document.getElementsByClassName("containerHarti3")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '3' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
+
 
 function ButonProgramPrezentari() {
 	window.location.href = "ProgramPrezentariUtilizator.html";
--- public/dataHartaVizitator.js
+++ public/dataHartaVizitator.js
@@ -1,11 +1,76 @@
 function initApp() {
 
-	document.getElementById('ButonSignOut').addEventListener('click', ButonSignOut, false);
+	document.getElementById('ButonLogin').addEventListener('click', ButonLogin, false);
 	document.getElementById('ButonContact').addEventListener('click', ButonContact, false);
 	document.getElementById('ButonAnunturi').addEventListener('click', ButonAnunturi, false);
 	document.getElementById('ButonProgramWorkshopuri').addEventListener('click', ButonProgramWorkshop, false);
-	
+	colecteazaButoane();
+	colecteazaButoane2();
+	colecteazaButoane3();
 }
+
+
+
+function colecteazaButoane() {
+
+	var container1 = document.getElementsByClassName("containerHarti1")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '1' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
+ 
+ 
+ function colecteazaButoane2() {
+ 
+	var container1 = document.getElementsByClassName("containerHarti2")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '2' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
+ 
+ function colecteazaButoane3() {
+ 
+	var container1 = document.getElementsByClassName("containerHarti3")[0];
+	for(var i = 1; i <= 9; i++) {
+		 let numar = i;
+		 firebase.database().ref('Standuri/' + '3' + numar).once("value").then(function(snapshot){
+			var numeClasa = snapshot.child("clasa").val();
+			var status = snapshot.child("status").val();
+			var buton = document.createElement("button");
+			buton.classList.add(numeClasa);
+			if(status == "liber")
+				buton.style.color ="green";
+			 else
+				 buton.style.color="red";
+			 buton.innerHTML = "STAND" + numar;
+			 container1.appendChild(buton); 
+		});
+	}
+ }
 
 function ButonProgramWorkshop() {
 	window.location.href = "ProgramWorkshopVizitator.html";
@@ -19,9 +84,8 @@ function ButonContact() {
 	window.location.href = "ContactVizitator.html";
 }
 
-function ButonSignOut() { 
-    firebase.auth().signOut();
-    window.location.href = "Homepage.html";
+function ButonLogin() { 
+	window.location.href = "Homepage.html";
 }
 window.onload = function() {
 	initApp();
