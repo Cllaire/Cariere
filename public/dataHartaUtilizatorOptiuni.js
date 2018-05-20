4187cd84a34bba0fb14016824a5b94e653891e00
alexandra-balan <alexandra.balan10@gmail.com>
2018-05-20 21:30:59 +0300

    Update dataHartaUtilizatorOptiuni.js

--- public/dataHartaUtilizatorOptiuni.js
+++ public/dataHartaUtilizatorOptiuni.js
@@ -6,6 +6,10 @@ function initApp() {
 	document.getElementById('ButonProgramWorkshopuri').addEventListener('click', ButonProgramWorkshopuri, false);
 	document.getElementById('butonFinal').addEventListener('click',updateButoane,false);
 	colecteazaButoane();
+	colecteazaButoane2();
+	colecteazaButoane3();
+	
+
 }
 
 function updateButoane(zi, nr_stand){
@@ -49,6 +53,46 @@ function colecteazaButoane() {
    }
 }
 
+
+function colecteazaButoane2() {
+
+   var container1 = document.getElementsByClassName("containerHarti2")[0];
+   for(var i = 1; i <= 9; i++) {
+	    let numar = i;
+		firebase.database().ref('Standuri/' + '2' + numar).once("value").then(function(snapshot){
+		   var numeClasa = snapshot.child("clasa").val();
+		   var status = snapshot.child("status").val();
+		   var buton = document.createElement("button");
+		   buton.classList.add(numeClasa);
+		   if(status == "liber")
+			   buton.style.color ="green";
+			else
+				buton.style.color="red";
+			buton.innerHTML = "STAND" + numar;
+			container1.appendChild(buton); 
+	   });
+   }
+}
+
+function colecteazaButoane3() {
+
+   var container1 = document.getElementsByClassName("containerHarti3")[0];
+   for(var i = 1; i <= 9; i++) {
+	    let numar = i;
+		firebase.database().ref('Standuri/' + '3' + numar).once("value").then(function(snapshot){
+		   var numeClasa = snapshot.child("clasa").val();
+		   var status = snapshot.child("status").val();
+		   var buton = document.createElement("button");
+		   buton.classList.add(numeClasa);
+		   if(status == "liber")
+			   buton.style.color ="green";
+			else
+				buton.style.color="red";
+			buton.innerHTML = "STAND" + numar;
+			container1.appendChild(buton); 
+	   });
+   }
+}
 
 function ButonProgramPrezentari() {
 	window.location.href = "ProgramPrezentariUtilizator.html";
